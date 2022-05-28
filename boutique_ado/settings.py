40d6f49a9e098bc9de783b52b4c5df6bc3b8dbab
RicardoIT-Web <rick_piedade@hotmail.com>
2022-05-28 18:18:05 +0000

    Deploy to heroku post aws setup

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,7 +178,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'EU (London) eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMIAN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
@@ -187,8 +187,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMIAN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMIAN}/{MEDIAFILES_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
